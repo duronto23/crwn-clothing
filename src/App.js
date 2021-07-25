d12eaa42507378383fac5ea7b7789d6abe416aef
duronto23 <duronto23@gmail.com>
2021-07-26 04:05:46 +0700

    Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { div } from 'prelude-ls';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.componet';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage} ></Route>
+        <Route path='/hats' component={HatsPage} ></Route>
+      </Switch>
     </div>
   );
 }
