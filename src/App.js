fcbd3ac25abce699d4bee3616f0574fa012a1dd1
duronto23 <duronto23@gmail.com>
2021-07-31 01:31:49 +0700

    imput and button component added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.componet';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 const HatsPage = () => (
@@ -19,6 +20,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} ></Route>
         <Route path='/shop' component={ShopPage} ></Route>
+        <Route path='/signin' component={SignInAndSignUpPage} ></Route>
       </Switch>
     </div>
   );
